c2559b03c675aee74cb033ff3a0aa65e824d0064
Rafael Barros <rafaelbarros.df@gmail.com>
2021-08-05 16:01:36 -0300

    :construction: Componentes, States e Props na prática - Parte 1

---
+++ instacat-app/src/App.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react'
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
+
+const App = () => {
+	const [like, setLike] = useState(0)
+
+	const increment = () => {
+		setLike(like + 1)
+	}
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.text}>{like}</Text>
+			<TouchableOpacity onPress={increment}>
+				<Text>Like</Text>
+			</TouchableOpacity>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	text: {
+		fontSize: 22,
+		color: '#00FF00',
+	},
+})
+
+export default App
