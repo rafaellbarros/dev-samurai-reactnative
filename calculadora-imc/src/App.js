9ead9f0894ead625f3819b6c555cda60847abb98
Rafael Barros <rafaelbarros.df@gmail.com>
2021-08-05 11:17:47 -0300

    :construction: Definindo a estrutura e estilização

--- calculadora-imc/src/App.js
+++ calculadora-imc/src/App.js
@@ -1,25 +1,50 @@
 import React, {Component} from 'react'
-import {View, Text, StyleSheet, Button} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
+import {Button, TextInput} from 'react-native-paper'
 
 export default class App extends Component {
-	digaOla = () => {
-		alert('Olá tudo bem Rafael?')
-	}
-
 	render() {
 		return (
-			<View style={sytles.container}>
-				<Button mode='contained' onPress={this.digaOla} title='Diga Olá para o Rafael.' />
+			<View style={styles.app}>
+				<Text style={styles.legenda}>Seu IMC</Text>
+
+				<View>
+					<Text style={styles.resultado}>25</Text>
+					<Text style={styles.diagnostico}>Normal</Text>
+				</View>
+				<View>
+					<TextInput style={styles.peso} />
+					<TextInput style={styles.altura} />
+					<Button>Calcular</Button>
+				</View>
 			</View>
 		)
 	}
 }
 
-const sytles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
+const styles = StyleSheet.create({
+	app: {
+		padding: 10,
+	},
+	legenda: {
+		textAlign: 'center',
+		fontWeight: 'bold',
+	},
+	resultado: {
+		textAlign: 'center',
+		fontSize: 22,
+		fontWeight: 'bold',
+	},
+	diagnostico: {
+		textAlign: 'center',
+		fontSize: 16,
+	},
+	peso: {
+		borderColor: '#000',
+		borderWidth: 1,
+	},
+	altura: {
+		borderColor: '#000',
+		borderWidth: 1,
 	},
 })
